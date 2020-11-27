01fe43ac941e60380310f3bc3d90e39418027e24
eluinstra <eluinstra@luin.dev>
2020-11-27 22:34:39 +0100

    implemented msp command added electron

--- msp.js
+++ msp.js
@@ -1,8 +1,9 @@
-const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('/dev/ttyUSB0', { baudRate: 115200 })
-
-const lineStream = port.pipe(new Readline())
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('/dev/ttyUSB0', { baudRate: 115200 });
+const lineStream = port.pipe(new Readline());
+const protocol = require('./protocol');
+const { FlowFlags } = require('typescript');
 
 const mspState =
 {
@@ -15,9 +16,21 @@ const mspState =
   MSP_COMMAND_RECEIVED: "command received"
 }
 
-var state = mspState.MSP_IDLE; 
+// const mspPort =
+// {
+//   port = new SerialPort('/dev/ttyUSB0', { baudRate: 115200 }),
+//     mspState_e c_state;
+//   uint8_t inBuf[MSP_PORT_INBUF_SIZE];
+//   uint16_t cmdMSP;
+//   uint8_t cmdFlags;
+//   uint_fast16_t offset;
+//   uint_fast16_t dataSize;
+//   uint8_t checksum2;
+// }
 
-port.on('data', function(data)
+var state = mspState.MSP_IDLE;
+
+port.on('data', function (data)
 {
   for (var i = 0; i < data.length; i++)
   {
@@ -25,6 +38,23 @@ port.on('data', function(data)
     parseMSPCommand(data.readInt8(i));
   }
 })
+
+function command(cmd, payload)
+{
+  const flag = 0;
+  const content = [].concat([flag],to16Bit(cmd),to16Bit(payload.size));// + payload;
+  return [].concat(protocol.mspCMDHeader.split("").map(ch => ch.charCodeAt(0)),content,[checksum(content)]);
+}
+
+function checksum(bytes)
+{
+  return bytes.reduce((crc, b) => crc8_dvb_s2(crc, b), 0);
+}
+
+function to16Bit(data)
+{
+  return [data & 0x00FF, data & 0xFF00];
+}
 
 function parseMSPCommand(ch)
 {
@@ -64,15 +94,48 @@ function parseMSPCommand(ch)
 // {
 //   console.log('Data:', data)
 // })
-port.write(toCharArray('$X<\x00\x64\x00\x00\x00\x8F'))
+const CMD = '$X<\x00\x64\x00\x00\x00\x8F';
+console.log(toByteBuffer(CMD));
+port.write(toByteBuffer(CMD));
 
-function toCharArray(str)
+const CMD1 = command(protocol.mspCMD.MSP_IDENT,"");
+console.log(CMD1);
+port.write(Buffer.from(CMD1));
+
+function number16AsHexStr(num)
 {
-  var arr = [];
-  for (var i = 0; i < str.length; i ++)
+  var high = num & 0xFF00;
+  var low = num & 0x00FF;
+  return ("0x" + high.toString(16).padStart(2,"0").toUpperCase() + low.toString(16).padStart(2,"0").toUpperCase());
+}
+
+function byteArrayAsHexStr(bytes)
+{
+  return bytes.reduce((acc, b) => acc + "0x" + b.toString(16).padStart(2,"0").toUpperCase(),"");
+}
+
+function toByteBuffer(str)
+{
+  return Buffer.from(str.split("").map(ch => ch.charCodeAt(0)));
+}
+
+function crc8_dvb_s2(crc, ch)
+{
+  crc = (crc ^ ch) % 256;
+  for (let i = 0; i < 8; ++i)
+    if ((crc & 0x80) != 0)
+      crc = ((crc << 1) ^ 0xD5) % 256;
+    else
+      crc = (crc << 1) % 256;
+  return crc;
+}
+
+function crc8_dvb_s2_update(crc, data, length)
+{
+  //return data.reduce((acc, b) => crc8_dvb_s2(crc,data[i]));
+  for (i = 0; i < length; i++)
   {
-    var ch = str.charCodeAt(i);
-    arr.push(ch);
+    crc = crc8_dvb_s2(crc,data[i]);
   }
-  return Buffer.from(arr);
+  return crc;
 }
---
+++ protocol.js
@@ -0,0 +1,26 @@
+const mspCMD =
+{
+  MSP_API_VERSION:   1,   //out message
+  MSP_FC_VARIANT:    2,   //out message
+  MSP_FC_VERSION:    3,   //out message
+  MSP_BOARD_INFO:    4,   //out message
+  MSP_BUILD_INFO:    5,   //out message
+  MSP_NAME:         10,   //out message          Returns user set board name - betaflight
+  MSP_SET_NAME:     11,   //in message           Sets board name - betaflight
+  //DEPRECATED -
+  MSP_IDENT:        100    //out message         mixerMode + multiwii version + protocol version + capability variable
+};
+
+const mspCMDHeader = '$X<';
+
+const mspMessageType =
+{
+  OUT: "<",
+  IN: ">",
+  ERROR: "!"
+}
+module.exports = {
+  mspCMD,
+  mspCMDHeader,
+  mspMessageType
+};
