63fe11228bec02bd2106cfd9175a186b4cd49af6
eluinstra <eluinstra@luin.dev>
2020-12-07 20:22:36 +0100

    enabled jsx

--- msp-app-s.tsx
+++ msp-app-s.tsx
@@ -21,30 +21,30 @@ const mspMsg = {
   checksum: 0
 }
 
-const mspFunctions = [];
-mspFunctions[1] = function(mspMsg) {
-  return "<div>" +
-    "<div>MSP_API_VERSION</div>" +
-    "<div>" + mspMsg.buffer.map(v => hexInt8(v)) + "</div>" +
-    "</div>"
+const mspOutputFunctions = [];
+mspOutputFunctions[mspCMD.MSP_API_VERSION] = function(mspMsg) {
+  return <div>
+    <div>MSP_API_VERSION</div>
+    <div>{mspMsg.buffer.map(v => hexInt8(v))}</div>
+    </div>
 }
-mspFunctions[2] = function(mspMsg) {
-  return "<div>" +
-    "<div>MSP_FC_VARIANT</div>" +
-    "<div>" + mspMsg.buffer.reduce((s, v) => s + String.fromCharCode(v),"") + "</div>" +
-    "</div>"
+mspOutputFunctions[mspCMD.MSP_FC_VARIANT] = function(mspMsg) {
+  return <div>
+    <div>MSP_FC_VARIANT</div>
+    <div>{mspMsg.buffer.reduce((s, v) => s + String.fromCharCode(v),"")}</div>
+    </div>
 }
-mspFunctions[3] = function(mspMsg) {
-  return "<div>" +
-    "<div>MSP_FC_VERSION</div>" +
-    "<div>" + mspMsg.buffer.map(v => hexInt8(v)) + "</div>" +
-    "</div>"
+mspOutputFunctions[mspCMD.MSP_FC_VERSION] = function(mspMsg) {
+  return <div>
+    <div>MSP_FC_VERSION</div>
+    <div>{mspMsg.buffer.map(v => hexInt8(v))}</div>
+    </div>
 }
-mspFunctions[100] = function(mspMsg) {
-  return "<div>" +
-    "<div>MSP_IDENT</div>" +
-    "<div>" + mspMsg.buffer.map(v => hexInt8(v)) + "</div>" +
-    "</div>"
+mspOutputFunctions[mspCMD.MSP_IDENT] = function(mspMsg) {
+  return <div>
+    <div>MSP_IDENT</div>
+    <div>{mspMsg.buffer.map(v => hexInt8(v))}</div>
+    </div>
 }
 
 const hexInt = (num, width) => num.toString(16).padStart(width,"0").toUpperCase();
@@ -78,7 +78,7 @@ port.on('data', function (data) {
     parseMSPCommand(data.readInt8(i))
     if (mspMsg.state == mspState.MSP_COMMAND_RECEIVED) {
       // console.log("SUCCESS")
-      mspOutput.innerHTML = "SUCCESS " + mspFunctions[mspMsg.cmd](mspMsg)
+      mspOutput.innerHTML = "SUCCESS " + mspOutputFunctions[mspMsg.cmd](mspMsg)
       mspMsg.state = mspState.MSP_IDLE
     }
   }
