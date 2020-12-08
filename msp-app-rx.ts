b28e6acd52448c6118db16edb8fba4a508c27109
eluinstra <eluinstra@luin.dev>
2020-12-08 21:38:25 +0100

    rearranged duplicate msp code

--- msp-app-rx.ts
+++ msp-app-rx.ts
@@ -1,28 +1,7 @@
-import { remote } from 'electron'
-const SerialPort = remote.require('serialport')
 import { fromEvent } from 'rxjs'
 import { map, filter } from 'rxjs/operators'
-import { mspCMD, mspCMDHeader, mspMessageType } from './protocol.js';
-
-const mspState = {
-  MSP_IDLE: "idle",
-  MSP_HEADER_START: "header start",
-  MSP_HEADER_X: "header x",
-  MSP_HEADER_V2_NATIVE: "header",
-  MSP_PAYLOAD_V2_NATIVE: "payload",
-  MSP_CHECKSUM_V2_NATIVE: "checksum",
-  MSP_COMMAND_RECEIVED: "command received",
-  MSP_ERROR_RECEIVED: "error received"
-}
-
-const mspMsg = {
-  state: mspState.MSP_IDLE,
-  flag: 0,
-  cmd: 0,
-  length: 0,
-  buffer: [],
-  checksum: 0
-}
+import { mspCMD } from './protocol.js'
+import { mspMsg, mspState, port, command, parseMSPCommand } from './msp.js'
 
 const mspFunctions = [];
 mspFunctions[100] = function() {
@@ -33,104 +12,11 @@ const hexInt = (num, width) => num.toString(16).padStart(width,"0").toUpperCase(
 
 const hexInt8 = num => hexInt(num & 0xFF,2);
 
-const hexInt16 = data => [data & 0x00FF, data & 0xFF00]
-
-const checksum = bytes => bytes.reduce((crc, b) => crc8_dvb_s2(crc, b), 0)
-
-function crc8_dvb_s2(crc, num) {
-  crc = (crc ^ num) & 0xFF
-  for (let i = 0; i < 8; i++)
-    if ((crc & 0x80) != 0)
-      crc = ((crc << 1) ^ 0xD5) & 0xFF
-    else
-      crc = (crc << 1) & 0xFF
-  return crc
-}
-
-const getFlag = data => data[0]
-
-const getCmd = data => (data[2] << 8) + data[1]
-
-const getLength = data => (data[4] << 8) + data[3]
-
-function command(cmd, payload) {
-  const flag = 0
-  const content = [].concat([flag],hexInt16(cmd),hexInt16(payload.size),payload)
-  return [].concat(mspCMDHeader.split("").map(ch => ch.charCodeAt(0)),content,[checksum(content)])
-}
-
-const port = new SerialPort('/dev/ttyUSB0', { baudRate: 115200 })
-
-// port.on('data', function (data) {
-//   for (let i = 0; i < data.length; i++) {
-//     parseMSPCommand(data.readInt8(i))
-//     if (mspMsg.state == mspState.MSP_COMMAND_RECEIVED) {
-//       // console.log("SUCCESS")
-//       mspOutput.innerHTML = "SUCCESS " + mspMsg.buffer.map(v => hexInt8(v))
-//       mspMsg.state = mspState.MSP_IDLE
-//     }
-//   }
-// })
-
-function parseMSPCommand(num) {
-  //console.log(num & 0xFF)
-  //console.log(hexInt8(num & 0xFF))
-  switch (mspMsg.state) {
-    case mspState.MSP_IDLE:
-      if (String.fromCharCode(num) == '$')
-        mspMsg.state = mspState.MSP_HEADER_START
-      break
-    case mspState.MSP_HEADER_START:
-      mspMsg.buffer = []
-      mspMsg.checksum = 0
-      if (String.fromCharCode(num) == 'X')
-        mspMsg.state = mspState.MSP_HEADER_X
-      break
-    case mspState.MSP_HEADER_X:
-      if (String.fromCharCode(num) == '>')
-        mspMsg.state = mspState.MSP_HEADER_V2_NATIVE
-      else if (String.fromCharCode(num) == '!')
-        mspMsg.state = mspState.MSP_IDLE
-      break
-    case mspState.MSP_HEADER_V2_NATIVE:
-      mspMsg.buffer.push(num & 0xFF)
-      mspMsg.checksum = crc8_dvb_s2(mspMsg.checksum, num)
-      if (mspMsg.buffer.length == 5) {
-        mspMsg.flag = getFlag(mspMsg.buffer)
-        mspMsg.cmd = getCmd(mspMsg.buffer)
-        mspMsg.length = getLength(mspMsg.buffer)
-        mspMsg.buffer = []
-        if (mspMsg.length > 0)
-          mspMsg.state = mspState.MSP_PAYLOAD_V2_NATIVE
-        else
-          mspMsg.state = mspState.MSP_CHECKSUM_V2_NATIVE
-      }
-      break
-    case mspState.MSP_PAYLOAD_V2_NATIVE:
-      mspMsg.buffer.push(num & 0xFF)
-      mspMsg.checksum = crc8_dvb_s2(mspMsg.checksum, num)
-      mspMsg.length--
-      if (mspMsg.length == 0)
-        mspMsg.state = mspState.MSP_CHECKSUM_V2_NATIVE
-      break
-    case mspState.MSP_CHECKSUM_V2_NATIVE:
-      if (mspMsg.checksum == (num & 0xFF))
-        mspMsg.state = mspState.MSP_COMMAND_RECEIVED
-      else
-        mspMsg.state = mspState.MSP_IDLE
-      break
-    default:
-      mspMsg.state = mspState.MSP_IDLE
-      break
-  }
-  //console.log("state " + mspMsg.state)
-}
-
 const mspButton = document.getElementById('mspButton')
 const mspInput = document.getElementById('mspInput')
 const mspOutput = document.getElementById('mspOutput')
-const printMspResult = mspMsg => mspOutput.innerHTML = "SUCCESS " + mspMsg.buffer.map(v => hexInt8(v))
 
+const printMspResult = mspMsg => mspOutput.innerHTML = "SUCCESS " + mspMsg.buffer.map(v => hexInt8(v))
 
 const clickStream = fromEvent(mspButton, 'click')
 .pipe(
--- msp.ts
+++ msp.ts
@@ -1,21 +1,19 @@
-import SerialPort from 'serialport';
-import {mspCMD, mspCMDHeader, mspMessageType} from './protocol';
+import { remote } from 'electron'
+const SerialPort = remote.require('serialport')
+import { mspCMDHeader } from './protocol';
 
-const port = new SerialPort('/dev/ttyUSB0', { baudRate: 115200 });
-
-const mspState =
-{
-  MSP_IDLE: "idle",
-  MSP_HEADER_START: "header start",
-  MSP_HEADER_X: "header x",
-  MSP_HEADER_V2_NATIVE: "header",
-  MSP_PAYLOAD_V2_NATIVE: "payload",
-  MSP_CHECKSUM_V2_NATIVE: "checksum",
-  MSP_COMMAND_RECEIVED: "command received"
+export const mspState = {
+  MSP_IDLE: 0,
+  MSP_HEADER_START: 1,
+  MSP_HEADER_X: 2,
+  MSP_HEADER_V2_NATIVE: 3,
+  MSP_PAYLOAD_V2_NATIVE: 4,
+  MSP_CHECKSUM_V2_NATIVE: 5,
+  MSP_COMMAND_RECEIVED: 6,
+  MSP_ERROR_RECEIVED: 7
 }
 
-const mspMsg =
-{
+export const mspMsg = {
   state: mspState.MSP_IDLE,
   flag: 0,
   cmd: 0,
@@ -24,139 +22,87 @@ const mspMsg =
   checksum: 0
 }
 
-const mspFunctions = [];
+const hexInt16 = data => [data & 0x00FF, data & 0xFF00]
 
-const hexInt = (num, width) => num.toString(16).padStart(width,"0").toUpperCase();
+const getFlag = data => data[0]
 
-const hexInt8 = num => hexInt(num,2);
+const getCmd = data => (data[2] << 8) + data[1]
 
-const int16ToHexStr = num => "0x" + hexInt8(num & 0xFF00) + hexInt8(num & 0x00FF);
+const getLength = data => (data[4] << 8) + data[3]
 
-const byteArrayToHexStr = bytes => bytes.reduce((acc, b) => acc + "0x" + b.toString(16).padStart(2,"0").toUpperCase(),"");
+const checksum = bytes => bytes.reduce((crc, b) => crc8_dvb_s2(crc, b), 0)
 
-const checksum = bytes => bytes.reduce((crc, b) => crc8_dvb_s2(crc, b), 0);
-
-const hexInt16 = data => [data & 0x00FF, data & 0xFF00];
-
-const toByteArray = str => str.split("").map(ch => ch.charCodeAt(0));
-
-const toByteBuffer = str => Buffer.from(toByteArray(str));
-
-const getFlag = data => data[0];
-
-const getCmd = data => (data[2] << 8) + data[1];
-
-const getLength = data => (data[4] << 8) + data[3];
-
-port.on('data', function (data)
-{
-  for (let i = 0; i < data.length; i++)
-  {
-    parseMSPCommand(data.readInt8(i));
-    if (mspMsg.state == mspState.MSP_COMMAND_RECEIVED)
-    {
-      console.log("SUCCESS");
-      mspMsg.state = mspState.MSP_IDLE;
-    }
-  }
-})
-
-function command(cmd, payload)
-{
-  const flag = 0;
-  const content = [].concat([flag],hexInt16(cmd),hexInt16(payload.size),payload);
-  return [].concat(mspCMDHeader.split("").map(ch => ch.charCodeAt(0)),content,[checksum(content)]);
+function crc8_dvb_s2(crc, num) {
+  crc = (crc ^ num) & 0xFF
+  for (let i = 0; i < 8; i++)
+    if ((crc & 0x80) != 0)
+      crc = ((crc << 1) ^ 0xD5) & 0xFF
+    else
+      crc = (crc << 1) & 0xFF
+  return crc
 }
 
-function parseMSPCommand(num)
-{
-  //console.log(num);
-  console.log(hexInt8(num));
-  switch (mspMsg.state)
-  {
+export const port = new SerialPort('/dev/ttyUSB0', { baudRate: 115200 })
+
+export function command(cmd, payload) {
+  const flag = 0
+  const content = [].concat([flag],hexInt16(cmd),hexInt16(payload.size),payload)
+  return [].concat(mspCMDHeader.split("").map(ch => ch.charCodeAt(0)),content,[checksum(content)])
+}
+
+export function parseMSPCommand(num) {
+  //console.log(num & 0xFF)
+  //console.log(hexInt8(num & 0xFF))
+  switch (mspMsg.state) {
     case mspState.MSP_IDLE:
       if (String.fromCharCode(num) == '$')
-        mspMsg.state = mspState.MSP_HEADER_START;
-      break;
+        mspMsg.state = mspState.MSP_HEADER_START
+      break
     case mspState.MSP_HEADER_START:
-      mspMsg.buffer = [];
-      mspMsg.checksum = 0;
+      mspMsg.buffer = []
+      mspMsg.checksum = 0
       if (String.fromCharCode(num) == 'X')
-        mspMsg.state = mspState.MSP_HEADER_X;
-      break;
+        mspMsg.state = mspState.MSP_HEADER_X
+      break
     case mspState.MSP_HEADER_X:
       if (String.fromCharCode(num) == '>')
-        mspMsg.state = mspState.MSP_HEADER_V2_NATIVE;
-      break;
+        mspMsg.state = mspState.MSP_HEADER_V2_NATIVE
+      else if (String.fromCharCode(num) == '!')
+        mspMsg.state = mspState.MSP_ERROR_RECEIVED
+      break
     case mspState.MSP_HEADER_V2_NATIVE:
-      mspMsg.buffer.push(num);
-      mspMsg.checksum = crc8_dvb_s2(mspMsg.checksum, num);
-      if (mspMsg.buffer.length == 5)
-      {
-        mspMsg.flag = getFlag(mspMsg.buffer);
-        mspMsg.cmd = getCmd(mspMsg.buffer);
-        mspMsg.length = getLength(mspMsg.buffer);
-        mspMsg.buffer = [];
+      mspMsg.buffer.push(num & 0xFF)
+      mspMsg.checksum = crc8_dvb_s2(mspMsg.checksum, num)
+      if (mspMsg.buffer.length == 5) {
+        mspMsg.flag = getFlag(mspMsg.buffer)
+        mspMsg.cmd = getCmd(mspMsg.buffer)
+        mspMsg.length = getLength(mspMsg.buffer)
+        mspMsg.buffer = []
         if (mspMsg.length > 0)
-          mspMsg.state = mspState.MSP_PAYLOAD_V2_NATIVE;
+          mspMsg.state = mspState.MSP_PAYLOAD_V2_NATIVE
         else
-          mspMsg.state = mspState.MSP_CHECKSUM_V2_NATIVE;
+          mspMsg.state = mspState.MSP_CHECKSUM_V2_NATIVE
       }
-      break;
+      break
     case mspState.MSP_PAYLOAD_V2_NATIVE:
-      mspMsg.buffer.push(num);
-      mspMsg.checksum = crc8_dvb_s2(mspMsg.checksum, num);
-      mspMsg.length--;
+      mspMsg.buffer.push(num & 0xFF)
+      mspMsg.checksum = crc8_dvb_s2(mspMsg.checksum, num)
+      mspMsg.length--
       if (mspMsg.length == 0)
-        mspMsg.state = mspState.MSP_CHECKSUM_V2_NATIVE;
-      break;
+        mspMsg.state = mspState.MSP_CHECKSUM_V2_NATIVE
+      break
     case mspState.MSP_CHECKSUM_V2_NATIVE:
       if (mspMsg.checksum == (num & 0xFF))
-        mspMsg.state = mspState.MSP_COMMAND_RECEIVED;
+        mspMsg.state = mspState.MSP_COMMAND_RECEIVED
       else
-        mspMsg.state = mspState.MSP_IDLE;
-      break;
+        mspMsg.state = mspState.MSP_IDLE
+      break
+    case mspState.MSP_ERROR_RECEIVED:
+      mspMsg.state = mspState.MSP_IDLE
+      break
     default:
-      mspMsg.state = mspState.MSP_IDLE;
-      break;
+      mspMsg.state = mspState.MSP_IDLE
+      break
   }
-  console.log("state " + mspMsg.state);
-}
-// port.on('readable', function ()
-// {
-//   console.log('Data:', port.read())
-// })
-// port.on('data', function (data)
-// {
-//   console.log('Data:', data)
-// })
-
-function crc8_dvb_s2(crc, num)
-{
-  crc = (crc ^ num) & 0xFF;
-  for (let i = 0; i < 8; ++i)
-    if ((crc & 0x80) != 0)
-      crc = ((crc << 1) ^ 0xD5) & 0xFF;
-    else
-      crc = (crc << 1) & 0xFF;
-  return crc;
-}
-
-function crc8_dvb_s2_update(crc, data, length)
-{
-  //return data.reduce((acc, b) => crc8_dvb_s2(crc,data[i]));
-  for (let i = 0; i < length; i++)
-    crc = crc8_dvb_s2(crc,data[i]);
-  return crc;
-}
-
-{
-  const CMD = '$X<\x00\x64\x00\x00\x00\x8F';
-  console.log(toByteBuffer(CMD));
-  //port.write(toByteBuffer(CMD));
-}
-{
-  const CMD = command(mspCMD.MSP_IDENT,[]);
-  console.log(Buffer.from(CMD));
-  port.write(Buffer.from(CMD));
+  //console.log("state " + mspMsg.state)
 }
