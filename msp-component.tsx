70163e02cc9fea20c3c888f0a4f6ea32db911a86
eluinstra <eluinstra@luin.dev>
2020-12-20 23:56:31 +0100

    improvements

--- msp-component.tsx
+++ msp-component.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { fromEvent, Subject } from 'rxjs'
-import { map, filter, startWith } from 'rxjs/operators'
+import { map, filter, tap, startWith } from 'rxjs/operators'
 import { useObservable } from './rx-tools'
 import { serialPort, command, mspCmdResponse$, mspMsg } from './msp-driver'
 import { mspOutputFunctions } from './msp-view'
@@ -16,7 +16,10 @@ export const MspComponent = (props) => {
     const mspInput = document.getElementById('mspInput')
     const click$ = fromEvent(mspButton, 'click')
       .pipe(
-        map(event => command(mspInput["value"],[])))
+        map(event => mspInput["value"]),
+        filter(value => value != ""),
+        map(value => command(value,[]))
+      )
     const sub = click$
       .subscribe(val => {
         serialPort.write(Buffer.from(val))
@@ -25,11 +28,12 @@ export const MspComponent = (props) => {
   });
   return <div>
       <NativeSelect id="mspInput">
+        <option aria-label="None" value="">None</option>
         {Object.keys(MspCmd).map(key =>
           <option key={MspCmd[key]} value={MspCmd[key]}>{key}</option>
         )}
       </NativeSelect>
-      <Button id="mspButton" color="primary">MSP Go</Button>
+      <Button id="mspButton" variant="contained">MSP Go</Button>
       <div>{mspOutput}</div>
     </div> 
 }
