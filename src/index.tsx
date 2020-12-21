fb3eedc1b18e7f7d88c2db035415f05200b3dbd1
eluinstra <eluinstra@luin.dev>
2020-12-21 22:01:18 +0100

    moved menu to drawer

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, CssBaseline, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography } from '@material-ui/core'
+import HomeTwoToneIcon from '@material-ui/icons/HomeTwoTone';
+import SettingsApplicationsTwoToneIcon from '@material-ui/icons/SettingsApplicationsTwoTone';
+import InfoTwoToneIcon from '@material-ui/icons/InfoTwoTone';
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,35 +15,83 @@ import { Home } from './home'
 import { MSP } from './msp'
 import { About } from './about'
 
-export const App = () => {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/msp">MSP</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/msp">
-            <MSP />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+const MyMenuItem = (props) => {
+  return <ListItem button key={props.text} component={Link} to={props.to}>
+      <ListItemIcon>{props.icon}</ListItemIcon>
+      <ListItemText primary={props.text} />
+    </ListItem>
 }
+
+export const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            MSP
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Router>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <Toolbar />
+          <div className={classes.drawerContainer}>
+            <List>
+              <MyMenuItem text="Home" to="/" icon={<HomeTwoToneIcon />} />
+              <MyMenuItem text="MSP" to="/msp" icon={<SettingsApplicationsTwoToneIcon />} />
+              <MyMenuItem text="About" to="/about" icon={<InfoTwoToneIcon />} />
+            </List>
+          </div>
+        </Drawer>
+        <main className={classes.content}>
+          <Switch>
+            <Route path="/msp">
+              <MSP />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </div>
+  )
+}
+
