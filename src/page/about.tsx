61f5cdd2d1fd759e9b52ba14064fa2081e1f1ca9
eluinstra <eluinstra@luin.dev>
2020-12-22 22:10:22 +0100

    added about info

--- src/page/about.tsx
+++ src/page/about.tsx
@@ -1,5 +1,18 @@
+import { remote } from 'electron'
 import React from 'react'
+import { Paper, Typography } from '@material-ui/core'
+const SerialPort = remote.require('serialport')
+
+const capitalize = s => s.charAt(0).toUpperCase() + s.slice(1)
 
 export const About = () => {
-  return <h2>About</h2>;
+  return <React.Fragment>
+      <h2>About</h2>
+      <Paper>
+        <Typography variant="body2" component="p">
+          {['node', 'electron', 'chrome'].map(a => <React.Fragment>{capitalize(a) + ': ' + process.versions[a]}<br /></React.Fragment>)}
+          SerialPort: {remote.require('serialport/package').version}
+        </Typography>
+      </Paper>
+    </React.Fragment>
 }
