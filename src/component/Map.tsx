f38184ab80690e0994a179672a185e03f35a9f74
eluinstra <eluinstra@luin.dev>
2020-12-31 20:04:58 +0100

    read point from file

--- src/component/Map.tsx
+++ src/component/Map.tsx
@@ -1,11 +1,47 @@
 import { stat } from "fs";
-import React, { useState } from "react";
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useEffect, useState } from "react"
+import { fromEvent } from 'rxjs'
+import { distinctUntilChanged, filter, map, takeUntil } from 'rxjs/operators'
+import { LatLngExpression, LatLngTuple, Polyline as LeafletPolyline, PolylineOptions } from 'leaflet'
+import { MapContainer, Polyline, TileLayer } from 'react-leaflet'
+import readline from 'readline'
+import fs from'fs'
 
-export const Map = () => {
+const isValid = v => !v.startsWith('nan')
+const lineToPoint = v => {
+  return {
+    lat: Number(v.substring(0, 9)),
+    lng: Number(v.substring(10, 18))
+  }
+}
+const comparePoints = (p, n) => p.lat === n.lat && p.lng === n.lng
+const pointToList = v => [v.lat, v.lng]
+
+const pointList = []
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('/home/user/test.txt')
+})
+
+const sub = fromEvent(rl, 'line')
+.pipe(
+  takeUntil(fromEvent(rl, 'close')),
+  filter(isValid),
+  map(lineToPoint),
+  distinctUntilChanged(comparePoints),
+  map(pointToList)
+)
+.subscribe(
+  v => pointList.push(v),
+  err => console.log("Error: %s", err),
+  () => console.log(pointList)
+)
+
+  export const Map = () => {
   const [state, setState] = useState({
     currentLocation: { lat: 53.21917, lng: 6.56667 },
     zoom: 12,
+    latlngs: pointList
   })
   return (
     <MapContainer center={state.currentLocation} zoom={state.zoom} style={{ width: '100%', height: '600px'}}>
@@ -13,6 +49,7 @@ export const Map = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
       />
+      <Polyline positions={state.latlngs} color={'red'} />
     </MapContainer>
   );
 }
