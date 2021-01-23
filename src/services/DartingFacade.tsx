6671359064831da98755b9022d31a582f8492586
disruptivenl <disruptivesolutionsnl@gmail.com>
2021-01-23 21:51:42 +0100

    Added 2 serial ports

---
+++ src/services/DartingFacade.tsx
@@ -0,0 +1,10 @@
+class DartingFacade {
+    constructor(serialPort1, serialPort2) {
+      //this.serialPort1 = serialPort1;
+      //this.serialPort2 = serialPort2;
+    }
+   
+    settingTheSerialPorts() {
+      //this.serialPort1.connect();   
+    }
+  }
