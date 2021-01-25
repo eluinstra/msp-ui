ab4eae47494670cb3fa3be35f1e57776e4f66a47
disruptivenl <disruptivesolutionsnl@gmail.com>
2021-01-25 16:19:20 +0100

    Different ApexChart Examples 2

---
+++ src/component/witmotion/ExampleChart2.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react'
+import { interval } from 'rxjs'
+import { map, sample } from 'rxjs/operators'
+import { llenAsync, lpushAsync, lrangeAsync, flushallAsync } from '@/services/dbcapturing'
+import ReactApexChart from 'react-apexcharts'
+
+function generateDayWiseTimeSeries(baseval, count, yrange) {
+  var i = 0;
+  var series = [];
+  while (i < count) {
+    var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+    series.push([baseval, y]);
+    baseval += 86400000;
+    i++;
+  }
+  return series;
+}
+
+// data for the sparklines that appear below header area
+var sparklineData = [47, 45, 54, 38, 56, 24, 65, 31, 37, 39, 62, 51, 35, 41, 35, 27, 93, 53, 61, 27, 54, 43, 19, 46];
+
+export const ExampleChart2 = props => {
+  const { driver, datasets } = props
+  const data = {
+    datasets: datasets
+  }
+
+  const options = {
+    series: [{
+      data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
+        min: 10,
+        max: 60
+      })
+    }],
+    options: {
+      chart: {
+        id: 'fb',
+        group: 'social',
+        type: 'line',
+        height: 160
+      },
+      colors: ['#008FFB'],
+      yaxis: {
+        labels: {
+          minWidth: 40
+        }
+      }
+    }
+
+  }
+  const options2 = {
+    series: [{
+      data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
+        min: 10,
+        max: 30
+      })
+    }],
+    options: {
+      chart: {
+        id: 'tw',
+        group: 'social',
+        type: 'line',
+        height: 160
+      },
+      colors: ['#546E7A'],
+      yaxis: {
+        labels: {
+          minWidth: 40
+        }
+      }
+    }
+
+  }
+  return (
+    <div>
+      <ReactApexChart options={options.options} series={options.series} type="line" height={320} />
+      <ReactApexChart options={options2.options} series={options2.series} type="line" height={320} />
+    </div>
+
+  )
+}
---
+++ src/component/witmotion/ExampleChart5.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react"
+import { Line } from "react-chartjs-2"
+import 'chartjs-plugin-streaming'
+import { interval } from "rxjs"
+import { filter, map, startWith, tap } from "rxjs/operators"
+import { sample } from "rxjs/operators"
+import { imuResponse$, registerPort, unregisterPort } from '@/component/imu/WitMotion/Driver'
+import { isOpen } from "@/component/serialport/SerialPortDriver"
+import ReactApexChart from 'react-apexcharts'
+import ApexCharts from 'apexcharts'
+
+const imuAngle = (h: number, l: number) => ((h.valueOf() << 8) | l.valueOf()) / 32768 * 180
+
+let XAXISRANGE = 777600000
+var TICKINTERVAL = 86400000
+
+let data = [];
+let lastDate = () => {0};
+
+export const ExampleChart5 = props => {
+  const { serialPort } = props
+  //const [data, updateData] = useState([1, 2, 3, 4, 5, 6]);
+  const [state] = useState({
+    options: {
+      chart: {
+        height: 350,
+        type: 'radialBar',
+        toolbar: {
+          show: true
+        }
+      }
+    },
+    series: [67],
+    colors: ["#20E647"],
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: "70%",
+          background: "#293450"
+        },
+        track: {
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            blur: 4,
+            opacity: 0.15
+          }
+        },
+        dataLabels: {
+          name: {
+            offsetY: -10,
+            color: "#fff",
+            fontSize: "13px"
+          },
+          value: {
+            color: "#fff",
+            fontSize: "30px",
+            show: true
+          }
+        }
+      }
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "vertical",
+        gradientToColors: ["#87D4F9"],
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: "round"
+    },
+    labels: ["Progress"]
+  })
+  
+  useEffect(() => {
+    //setInterval(() => {
+
+      // ApexCharts.exec('realtime', 'updateSeries', [{
+      //   data: data
+      // }])
+      
+      
+    //}, 1000);
+    
+    
+
+  }, [data])
+  return (
+    <ReactApexChart options={state.options} series={state.series} type="radialBar" height={350} />
+  )
+}
