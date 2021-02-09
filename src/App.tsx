d9b21b2a204c6a5daaabf99c6f75e47b529bfc4a
DisruptiveNL <disruptivesolutionsnl@gmail.com>
2021-02-09 23:28:20 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ import { RealTimeChart } from '@/component/witmotion/RealTimeChart'
 import { ExampleChart5 } from '@/component/witmotion/ExampleChart5'
 import { ExampleChart6 } from '@/component/witmotion/ExampleChart6'
 import { SerialRTChartPage } from '@/page/SerialRTChartPage'
-import { SerialRTChartPage2 } from '@/page/SerialRTChartPage2'
 import { MainContainer } from '@/component/witmotion/MainContainer'
 import { AudioPlayExample } from '@/component/witmotion/AudioPlayExample'
 import { WitMotion } from '@/page/WitMotion'
@@ -288,8 +287,7 @@ const MSPRouter = props => {
         <ExampleChart6 />
       </Route>
       <Route path="/serialrtchart">
-        <SerialRTChartPage id={1} serialPort={serialPort1} />
-        <SerialRTChartPage id={2} serialPort={serialPort2} />
+        <SerialRTChartPage id={1} serialPort1={serialPort1} serialPort2={serialPort2}  />
       </Route>
       <Route path="/audioplayexample">
         <AudioPlayExample />
--- src/page/SerialRTChartPage.tsx
+++ src/page/SerialRTChartPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { SerialChart } from '@/component/witmotion/SerialChart'
+import { SerialChartCOM1 } from '@/component/witmotion/SerialChartCOM1'
+import { SerialChartCOM2 } from '@/component/witmotion/SerialChartCOM2'
 
 export const SerialRTChartPage = props => {
-  const { id, serialPort } = props
+  const { id, serialPort1, serialPort2 } = props
   return (
     <React.Fragment>
       <h2>SerialRTChart</h2>
-      <SerialChart id={id} serialPort={serialPort} height={100}/>
+      <SerialChartCOM1 serialPort={serialPort1} height={100}/>
+      <SerialChartCOM2 serialPort={serialPort2} height={100}/>
     </React.Fragment>
   )
 }
